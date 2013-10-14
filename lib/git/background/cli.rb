cf1c6d52ca796ba4dc7765f8aff65f67dc171018
rhysd <lin90162@yahoo.co.jp>
2013-10-15 01:32:55 +0900

    parse options and show help if needed

---
+++ lib/git/background/cli.rb
@@ -0,0 +1,36 @@
+# encoding: utf-8
+
+require 'optparse'
+
+module Git
+  module Background
+    module CLI extend self
+
+      def start
+        if ARGV.empty?
+          STDERR.puts help
+        else
+          notifier = parse_argv!
+          Process::daemon
+          # Git::Background::Runner.new
+        end
+      end
+
+      def parse_argv!
+        notifier = nil
+        opt = OptionParser.new
+        opt.on('--[no-]notify[=VAL]') do |n|
+          notifier = n if n.class == String
+        end
+        opt.parse!(ARGV)
+        notifier
+      end
+
+      def help
+          'usage: git background [--[no-]notify=[notifier]] commands...'
+      end
+
+    end
+  end
+end
+
