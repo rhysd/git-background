70075f6d586e1cc1317f479d6b432ffa935f8337
rhysd <lin90162@yahoo.co.jp>
2013-10-15 02:50:53 +0900

    require terminal-notifier, remove useress requiring

---
+++ lib/git/background/notifier_ext.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'git/background/notifier_ext/osx_notification'
+require 'git/background/notifier_ext/tmux'
+
--- lib/git/background/notifier_ext/osx_notification.rb
+++ lib/git/background/notifier_ext/osx_notification.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+require 'terminal-notifier'
+
 module Notifier
   module OsxNotification extend self
 
